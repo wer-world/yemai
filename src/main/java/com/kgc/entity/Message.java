39c08dc8acf699cd58a83ceee9597728bf2ae24e
idiotzzz <2537876081@qq.com>
2024-03-18 10:38:17 +0800

    冯天威第一次提交

---
+++ src/main/java/com/kgc/entity/Message.java
@@ -0,0 +1,36 @@
+package com.kgc.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 消息类
+ *
+ * @Author: 魏小可
+ * @Date: 2024-03-16-22:25
+ */
+@Data
+@ToString
+@AllArgsConstructor
+public class Message {
+    private String code;
+    private String message;
+    private Object data;
+
+    public static Message success() {
+        return new Message("200", "操作成功", null);
+    }
+
+    public static Message success(Object data) {
+        return new Message("200", "操作成功", data);
+    }
+
+    public static Message error() {
+        return new Message("200", "操作失败", null);
+    }
+
+    public static Message error(String message) {
+        return new Message("200", message, null);
+    }
+}
---
+++ src/main/java/com/kgc/util/Log.java
@@ -0,0 +1,40 @@
+package com.kgc.util;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+/**
+ * 日志增强类
+ *
+ * @Author: 魏小可
+ * @Date: 2024-03-16-22:27
+ */
+@Aspect
+@Component
+public class Log {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Pointcut(value = "execution(* com.kgc..*.*(..))")
+    public void pointCut(){}
+
+    @Around("pointCut()")
+    public Object around(ProceedingJoinPoint joinPoint){
+        try {
+            logger.info("Log around class:" + joinPoint.getSignature() + " start...");
+            logger.debug("Log around class:" + joinPoint.getSignature() + ",method:" + joinPoint.getTarget() + ",method params:" + Arrays.toString(joinPoint.getArgs()));
+            Object result = joinPoint.proceed();
+            logger.debug("Log around class:" + joinPoint.getSignature() + ",method:" + joinPoint.getTarget() + ",method params:" + Arrays.toString(joinPoint.getArgs()) + ",result:" + result);
+            logger.info("Log around class:" + joinPoint.getSignature() + " end...");
+            return result;
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
