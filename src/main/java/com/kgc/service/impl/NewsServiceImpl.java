e8136375d706a8a8e52186c8a02ec91c78fc0c35
war-world <wei2236025735@qq.com>
2024-03-21 08:28:14 +0800

    项目第三次整合，版本0.0.4base版本，详细见explain.md说明文档

--- src/main/java/com/kgc/service/impl/NewsServiceImpl.java
+++ src/main/java/com/kgc/service/impl/NewsServiceImpl.java
@@ -13,10 +13,10 @@ import java.util.Map;
 @Service
 public class NewsServiceImpl implements NewsService {
     @Autowired
-    private NewsDao newsMapper;
+    private NewsDao newsDao;
     @Override
     public Message getNewsList(Map map) {
-        List<News> newsList = newsMapper.getNewsList(map);
+        List<News> newsList = newsDao.getNewsList(map);
         if (newsList != null&&newsList.size() != 0){
             return Message.success(newsList);
         }
@@ -26,7 +26,7 @@ public class NewsServiceImpl implements NewsService {
     @Override
     public Message getNewsTotalCount() {
         long count = 0;
-        count =  newsMapper.getNewsTotalCount();
+        count =  newsDao.getNewsTotalCount();
         if (count != 0){
             return Message.success(count);
         }
@@ -42,7 +42,7 @@ public class NewsServiceImpl implements NewsService {
             return Message.error("请输入内容");
         }
         int count = 0;
-        count = newsMapper.addNews(news);
+        count = newsDao.addNews(news);
         if (count != 1){
             return Message.error();
         }
@@ -51,7 +51,7 @@ public class NewsServiceImpl implements NewsService {
 
     @Override
     public Message delNewsById(int id) {
-        int count = newsMapper.delNewsById(id);
+        int count = newsDao.delNewsById(id);
         if (count != 1){
             return Message.error();
         }
@@ -66,7 +66,7 @@ public class NewsServiceImpl implements NewsService {
         if (news.getContent() == null || "".equals(news.getContent())){
             return Message.error("请输入内容");
         }
-        int count = newsMapper.modifyNewsById(news);
+        int count = newsDao.modifyNewsById(news);
         if (count != 1){
             return Message.error();
         }
@@ -75,7 +75,7 @@ public class NewsServiceImpl implements NewsService {
 
     @Override
     public Message getNewsById(int id) {
-            News news = newsMapper.getNewsById(id);
+            News news = newsDao.getNewsById(id);
         return Message.success(news);
     }
 
@@ -84,7 +84,7 @@ public class NewsServiceImpl implements NewsService {
         if (title == null){
             return Message.error("请输入标题");
         }
-        News news = newsMapper.getNewsByTitle(title);
+        News news = newsDao.getNewsByTitle(title);
         if (news != null){
             return Message.error("该标题已存在");
         }
